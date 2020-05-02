008ec1cf12cdf59c794026c3e2efc2b9e8314dc9
Stanley <stanley.traub@gmail.com>
2020-05-02 14:22:16 -0700

    Refactored mutations

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,7 +1,14 @@
 import uuidv4 from 'uuid/v4'
 
 const Mutation = {
-    createUser(parent, args, { prisma }, info) {
+    async createUser(parent, args, { prisma }, info) {
+        const emailTaken = await prisma.exists(User({ email: args.data.email }))
+
+        if (emailTaken) throw new Error('Email taken')
+
+        return prisma.muation.createUser({ data: args.data}, info)
+
+      
         // const emailTaken = db.users.some((user) => user.email === args.data.email)
 
         // if (emailTaken) {
@@ -17,27 +24,16 @@ const Mutation = {
 
         // return user
     },
-    deleteUser(parent, args, { db }, info) {
-        const userIndex = db.users.findIndex((user) => user.id === args.id)
+    async deleteUser(parent, args, { prisma }, info) {
+        const userExists = await prisma.exists.User({id : args.id})
 
-        if (userIndex === -1) {
-            throw new Error('User not found')
-        }
+        if (!userExists) throw new Error('User not found')
 
-        const deletedUsers = db.users.splice(userIndex, 1)
-
-        db.posts = db.posts.filter((post) => {
-            const match = post.author === args.id
-
-            if (match) {
-                db.comments = db.comments.filter((comment) => comment.post !== post.id)
+        return prisma.mutation.deleteUser({ 
+            where: {
+                id: args.id
             }
-
-            return !match
-        })
-        db.comments = db.comments.filter((comment) => comment.author !== args.id)
-
-        return deletedUsers[0]
+        }, info)
     },
     updateUser(parent, args, { db }, info) {
         const { id, data } = args
